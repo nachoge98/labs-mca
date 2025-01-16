0e46960461e886d1accf27e00fa91627acac9919
nachoge98 <nachoge98@gmail.com>
2025-01-16 15:46:19 +0100

    more changes

---
+++ tools/install_oss_cad.sh
@@ -0,0 +1,5 @@
+#Script to download and untar the oss-cad-suite tools:
+
+wget "https://github.com/YosysHQ/oss-cad-suite-build/releases/download/2025-01-16/oss-cad-suite-linux-x64-20250116.tgz"
+tar xvzf oss-cad-suite-linux-x64-20250116.tgz
+rm oss-cad-suite-linux-x64-20250116.tgz
